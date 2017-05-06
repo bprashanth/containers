0a914a21488d221fe5a36be52751875d61ff414b
bprashanth <beeps@google.com>
2017-05-06 13:14:26 +0530

    Tweaks to Dockerfile to copy config.js

--- ghost/config.js
+++ ghost/config.js
@@ -12,7 +12,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+      url: 'http://localhost:8080',
         mail: {},
         database: {
             client: 'sqlite3',
--- ghost/entrypoint.sh
+++ ghost/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 mkdir -p "${GHOST_CONTENT}/content/themes"
-cp "${GHOST_SOURCE}/config.js" "${GHOST_CONTENT}/config.js"
+# simply mounting it also does the trick but changes permissions on the host
+cp "${GHOST_SOURCE}/staging_config.js" "${GHOST_CONTENT}/config.js"
 rsync -aI "${GHOST_SOURCE}/content/themes/" "${GHOST_CONTENT}/content/themes/"
 chown -R user:user "$GHOST_CONTENT"
 exec "$@"
