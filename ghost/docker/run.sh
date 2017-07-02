61834bad9b89e4d4e1f35767b9ecdd03d5eb4951
bprashanth <beeps@google.com>
2017-07-02 17:15:54 +0530

    Tags: modified, but still needs manual enable of public api

--- ghost/docker/run.sh
+++ ghost/docker/run.sh
@@ -3,5 +3,5 @@ docker run -d --env NODE_ENV=development --name ghost -p 80:2368 \
   -v $HOME/ghost:/var/lib/ghost \
   -v $(dirname `pwd`)/kubernetes/themes:/usr/src/ghost/content/themes \
   -v $(dirname `pwd`)/kubernetes/config.js:/usr/src/ghost/staging_config.js \
-  bprashanth/ghost:0.3
+  bprashanth/ghost:0.4
 
