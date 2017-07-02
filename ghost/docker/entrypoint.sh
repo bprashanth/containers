9614276e2a43a0cf99a47f17c2b9e268af4856ba
bprashanth <beeps@google.com>
2017-07-02 12:39:35 +0530

    Added new ghost files
    Forgto to add files in the list "kube refactor" commit

---
+++ ghost/docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ -f "${GHOST_SOURCE}/staging_config.js" ]; then
+  # simply mounting it also does the trick but changes permissions on the host
+  cp "${GHOST_SOURCE}/staging_config.js" "${GHOST_CONTENT}/config.js"
+fi
+mkdir -p "${GHOST_CONTENT}/content/themes"
+rsync -aI "${GHOST_SOURCE}/content/themes/" "${GHOST_CONTENT}/content/themes/"
+chown -R user:user "$GHOST_CONTENT"
+exec "$@"
