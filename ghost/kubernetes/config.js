0c0a671741bfc4f48a3027c1ba55f89cd9c4246c
bprashanth <prashanthseven@gmail.com>
2019-04-30 03:52:05 +0000

    Notwelcome.in to config.js

--- ghost/kubernetes/config.js
+++ ghost/kubernetes/config.js
@@ -12,7 +12,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-      url: 'https://kdump.in',
+      url: 'https://notwelcome.in',
         mail: {},
         database: {
             client: 'sqlite3',
